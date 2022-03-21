7679e2f559fc0d07bdc0b991d1fdf60146eeb939
Tobias Conn <tobias.conn@gmail.com>
2022-03-21 16:55:57 -0400

    added environment vars

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -12,7 +13,6 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
 //connect/setup mongoose
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
@@ -22,8 +22,7 @@ const userSchema = new mongoose.Schema ({
     password: String,
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 //define User model
 const User = new mongoose.model("User", userSchema);
